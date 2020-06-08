2a7885e80eab4502f0f574e6c4b0b1b3dff55ab6
tientp <Tran.Phuoc.Tien@steadfasttech.com.au>
2020-06-08 17:05:26 +0700

    add-contract-api

---
+++ WebAPI/Controllers/ContractController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataService.DTOs.ContractDTO;
+using DataService.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using DataService.Services.ContractService;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContractController : ControllerBase
+    {
+        private readonly DisneyDB _dbContext;
+
+        public ContractController(DisneyDB dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IActionResult> Create(Contract_Rq rq)
+        {
+            CreateTransactionService contractService = new CreateTransactionService(_dbContext);
+            Contract_Rs rs = await contractService.Execute(rq);
+
+            return Ok(rs.ResponseCode);
+        }
+    }
+}
